e18fc6b92ff21fc6e8a906c1514cfdf8f137b11e
srinidhieadara-07 <srinidhieadara@gmail.com>
2026-04-10 14:08:52 +0530

    UC4: Print banner using array and loop

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,19 +1,17 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String line1 = String.join("", "  *****          *****          *******         ******* ");
-        String line2 = String.join("", "*       *      *       *       *        *      *         ");
-        String line3 = String.join("", "*       *      *       *       *        *      *         ");
-        String line4 = String.join("", "*       *      *       *       *******          *******");
-        String line5 = String.join("", "*       *      *       *       *                        *");
-        String line6 = String.join("", "*       *      *       *       *                        *");
-        String line7 = String.join("", "  *****          *****         *                *******");
+        String[] bannerLines = {
+                String.join("", "   *****           *****          *******      ******* "),
+                String.join("", "*         *     *         *      *        *   *         "),
+                String.join("", "*         *     *         *      *        *   *         "),
+                String.join("", "*         *     *         *      ********      *******"),
+                String.join("", "*         *     *         *      *                    *"),
+                String.join("", "*         *     *         *      *                    *"),
+                String.join("", "   *****           *****         *             *******")
+        };
 
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        System.out.println(line6);
-        System.out.println(line7);
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
