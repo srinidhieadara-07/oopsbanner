01103624fb0a0f58347c0ba94c268340eec283c7
srinidhieadara-07 <srinidhieadara@gmail.com>
2026-04-10 13:27:35 +0530

    UC2: Print OOPS banner with manual lines

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -2,4 +2,22 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
+}public class OOPSBannerApp {
+    public static void main(String[] args) {
+        String line1 = "   *****           *****         *******        ******* ";
+        String line2 = "*         *     *         *    *         *     *         ";
+        String line3 = "*         *     *         *    *         *     *         ";
+        String line4 = "*         *     *         *    * *******        *******";
+        String line5 = "*         *     *         *    *                       *";
+        String line6 = "*         *     *         *    *                       *";
+        String line7 = "   *****           *****       *                *******";
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
 }
